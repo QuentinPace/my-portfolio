d96831c54607315b1f00b52845b96ec20e6172eb
QuentinPace <quentinepace@gmail.com>
2024-09-06 13:19:47 -0400

    styling

--- js-scripts/event-listeners.js
+++ js-scripts/event-listeners.js
@@ -1,6 +1,3 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM fully loaded and parsed");
-    console.log('porfavor-2')
-    const header = document.querySelector('h1')
-    header.innerText = 'works'
 });
